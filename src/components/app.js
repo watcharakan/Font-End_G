28cfac65522492ec579d6134f26e191ee4c945eb
watcharakan <job.2538@gmail.com>
2017-11-04 18:45:50 +0700

    cal

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Page, Panel, Input, Table, TableHead, TableBody, TableRow, Textarea,Breadcrumbs, Button  } from 'react-blur-admin';
+import PieChart from 'react-minimal-pie-chart';
+import * as FontAwesome from 'react-icons/lib/fa'
+
+
 export default class App extends Component {
 
   render() {
@@ -7,7 +11,19 @@ export default class App extends Component {
       <div >
 
       <Panel>React simple starter</Panel>
+      <FontAwesome.FaBeer />
 
+
+      <PieChart
+      radius = {20}
+      paddingAngle
+      lineWidth = {30}
+        data={[
+          { value: 10, key: 1, color: '#E38627' },
+          { value: 15, key: 2, color: '#C13C37' },
+          { value: 20, key: 3, color: '#6A2135' },
+        ]}
+      />
 
 
       <Input
