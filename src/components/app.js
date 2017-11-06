6f85070f51c0ed21b5dba28e9ae9ff18495e90ed
watcharakan <job.2538@gmail.com>
2017-11-06 11:25:49 +0700

    Revert "cal"
    This reverts commit 28cfac65522492ec579d6134f26e191ee4c945eb.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import { Page, Panel, Input, Table, TableHead, TableBody, TableRow, Textarea,Breadcrumbs, Button, Tab, Tabs  } from 'react-blur-admin';
 import PieChart from 'react-minimal-pie-chart';
 import * as FontAwesome from 'react-icons/lib/fa'
 
 
+=======
+import { Page, Panel, Input, Table, TableHead, TableBody, TableRow, Textarea,Breadcrumbs, Button  } from 'react-blur-admin';
+>>>>>>> parent of 28cfac6... cal
 export default class App extends Component {
 
   render() {
@@ -11,19 +15,7 @@ export default class App extends Component {
       <div >
 
       <Panel>React simple starter</Panel>
-      <FontAwesome.FaBeer />
 
-
-      <PieChart
-      radius = {20}
-      paddingAngle
-      lineWidth = {30}
-        data={[
-          { value: 10, key: 1, color: '#E38627' },
-          { value: 15, key: 2, color: '#C13C37' },
-          { value: 20, key: 3, color: '#6A2135' },
-        ]}
-      />
 
 
       <Input
