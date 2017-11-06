c943a391047abc0d9b7da12b4c4ca42840350ee9
watcharakan <job.2538@gmail.com>
2017-11-06 11:23:40 +0700

    f

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Page, Panel, Input, Table, TableHead, TableBody, TableRow, Textarea,Breadcrumbs, Button  } from 'react-blur-admin';
+import { Page, Panel, Input, Table, TableHead, TableBody, TableRow, Textarea,Breadcrumbs, Button, Tab, Tabs  } from 'react-blur-admin';
 import PieChart from 'react-minimal-pie-chart';
 import * as FontAwesome from 'react-icons/lib/fa'
 
@@ -32,7 +32,18 @@ export default class App extends Component {
                label='Success'
                />
 <Textarea />
+<Tabs
+         align='top'
+         startTab={2} >
+         <Tab title='Travel'>
+           <h2>HEY</h2>
 
+         </Tab>
+         <Tab title='Tabs 2'>
+           <h2>The Tabs Component</h2>
+           
+         </Tab>
+       </Tabs>
 <Button type='warning' size='xm' />
 
                     <Page>
