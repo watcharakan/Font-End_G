7b6e73b60dbd59358ff8531c17b3eb960ca2e156
watcharakan <job.2538@gmail.com>
2017-11-07 18:46:35 +0700

    N

--- src/components/app.js
+++ src/components/app.js
@@ -1,49 +1,20 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { Page, Panel, Input, Table, TableHead, TableBody, TableRow, Textarea,Breadcrumbs, Button, Tab, Tabs  } from 'react-blur-admin';
-import PieChart from 'react-minimal-pie-chart';
-import * as FontAwesome from 'react-icons/lib/fa'
+import { Page, Panel, Button, ProgressBar } from 'react-blur-admin';
 
-
-=======
-import { Page, Panel, Input, Table, TableHead, TableBody, TableRow, Textarea,Breadcrumbs, Button  } from 'react-blur-admin';
->>>>>>> parent of 28cfac6... cal
 export default class App extends Component {
-
   render() {
     return (
-      <div >
 
-      <Panel>React simple starter</Panel>
-
-
-
-      <Input
-               onChange={e => this.onTextChange('success', e)}
-               onValidate={e => true}
-               label='Success'
-               />
-<Textarea />
-<Tabs
-         align='top'
-         startTab={2} >
-         <Tab title='Travel'>
-           <h2>HEY</h2>
-
-         </Tab>
-         <Tab title='Tabs 2'>
-           <h2>The Tabs Component</h2>
-           
-         </Tab>
-       </Tabs>
-<Button type='warning' size='xm' />
-
-                    <Page>
-                    <Panel title='The Team'>
-                    Lorem Ipsum
-                    </Panel>
-                    </Page>
-    </div>
+      <div>
+      <Panel>
+      React simple starter
+      </Panel>
+      <Button type='default' size='lg' />
+        <Panel>
+        
+      <ProgressBar type='primary' percentage={[15]} label={"15 Complete"}/>
+       </Panel>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import Register from './components/register';
 
 import App from './components/app';
-
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Register />
+    <App />
   </Provider>
   , document.querySelector('.container'));
